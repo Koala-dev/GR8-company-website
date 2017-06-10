167addca4b0b35d713486363d6431a256c164423
Ryan Guo <guohao_lin@msn.com>
2017-06-10 06:16:24 -0700

    app
    Responsive web design

---
+++ app/scripts/main.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+  // Your custom JavaScript goes here
+
+  /**
+   * narbar
+   */
+   $('.ui.navbar .search-icon > i')
+    .click(function(){
+      let icon = $(this)
+      if (icon.text() === 'search') {
+        icon.text('close').parents('.content').addClass('searching')
+      } else {
+        icon.text('search').parents('.content').removeClass('searching')
+      }
+    })
+})();
